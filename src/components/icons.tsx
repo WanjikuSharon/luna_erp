57627246979511dcfe0fa0388e71e83fb9f21dd9
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-11-11 10:14:26 +0300

    feat: add new logo image for Luna Industries

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,11 +1,11 @@
-import type { SVGProps } from 'react';
+import type { HTMLAttributes } from 'react';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 const newLogoUrl = 'https://i.postimg.cc/9FzKTLkD/WhatsApp_Image_2025-10-15_at_00.18.06_514d4d8f.jpg';
 
 
-export function Logo({ className, ...props }: SVGProps<SVGSVGElement> & { className?: string }) {
+export function Logo({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div className={cn("relative", className)} {...props}>
         <Image
