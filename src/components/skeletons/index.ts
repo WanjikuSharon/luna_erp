f09e3aa1651bb7c39eb457661aae8e6c7a4c11a9
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-11-14 16:49:17 +0300

    feat: add skeleton components index file to export all skeletons

---
+++ src/components/skeletons/index.ts
@@ -0,0 +1,5 @@
+// src/components/skeletons/index.ts
+export { TableSkeleton } from './TableSkeleton';
+export { CardSkeleton } from './CardSkeleton';
+export { DashboardSkeleton } from './DashboardSkeleton';
+export { FormSkeleton } from './FormSkeleton';
