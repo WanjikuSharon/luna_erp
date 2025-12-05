fc76fdff634d743a6794c624262e7b5a2ee998da
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-12-05 13:57:48 +0300

    feat: add AdminProductsPage component to display Products Inventory

---
+++ src/app/(app)/admin/products/page.tsx
@@ -0,0 +1,8 @@
+// src/app/(app)/admin/products/page.tsx
+'use client';
+
+import ProductsInventoryView from '@/components/inventory/ProductsInventoryView';
+
+export default function AdminProductsPage() {
+  return <ProductsInventoryView />;
+}
