7f24b681dfaf6c7f7e264661c8984b4c8e0ff131
WanjikuSharon <sharonwanjiku292@gmail.com>
2025-12-05 13:58:14 +0300

    feat: create OperationsProductsPage component to display Products Inventory

---
+++ src/app/(app)/operations/products/page.tsx
@@ -0,0 +1,8 @@
+// src/app/(app)/operations/products/page.tsx
+'use client';
+
+import ProductsInventoryView from '@/components/inventory/ProductsInventoryView';
+
+export default function OperationsProductsPage() {
+  return <ProductsInventoryView />;
+}
